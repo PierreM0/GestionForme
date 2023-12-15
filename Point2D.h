eaba04727ab4f6c5ceb311f14195af147be67ed9
Devin PierreEmmanuel <pierre-emmanuel.devin2@etu.univ-lorraine.fr>
2023-12-15 20:32:45 +0100

    Vecteurs et des points

---
+++ Point2D.h
@@ -0,0 +1,11 @@
+//
+// Created by pierrem on 15/12/23.
+//
+
+#ifndef POINT2D_H
+#define POINT2D_H
+
+#include "Vecteur2D.h"
+typedef Vecteur2D Point2D;
+
+#endif //POINT2D_H
---
+++ Vecteur2D.h
@@ -0,0 +1,101 @@
+//
+// Created by pierrem on 07/12/23.
+//
+
+#ifndef VECTEUR2D_H
+#define VECTEUR2D_H
+
+//
+// Created by pierrem on 06/12/23.
+//
+
+#include <cmath>
+#include <sstream>
+#include <string>
+
+
+using namespace std;
+
+class Vecteur2D
+{
+public:
+    double x, y;
+    static double constexpr PI = 3.1415926535;
+
+    Vecteur2D(): x(0), y(0) {}
+    Vecteur2D(const double& x, const double& y): x(x), y(y)
+    {
+    }
+
+    static const Vecteur2D creeVecteurUnitaire(const double& alpha)
+    {
+        return Vecteur2D(cos(alpha), sin(alpha));
+    }
+
+    explicit operator string() const
+    {
+        ostringstream o;
+        o << "(" << x << ", " << y << ")";
+        return o.str();
+    }
+
+    Vecteur2D operator +(const Vecteur2D& v) const
+    {
+        return Vecteur2D{x + v.x, y + v.y};
+    }
+
+    const Vecteur2D& operator +=(const Vecteur2D& v)
+    {
+        x += v.x;
+        y += v.y;
+        return *this;
+    }
+
+    Vecteur2D operator-() const
+    {
+        return Vecteur2D{-x, -y};
+    }
+
+    Vecteur2D operator-(const Vecteur2D& v) const
+    {
+        return Vecteur2D{x - v.x, y - v.y};
+    }
+
+    Vecteur2D operator*(const double& d) const
+    {
+        return Vecteur2D{d * x, d * y};
+    }
+
+    double operator*(const Vecteur2D& v) const
+    {
+        return x * v.x + y * v.y;
+    }
+
+    double normeCarree() const
+    {
+        return *this * *this;
+    }
+
+    double norme() const
+    {
+        return sqrt(normeCarree());
+    }
+
+    bool operator ==(const Vecteur2D& v) const
+    {
+        return x == v.x && y == v.y;
+    }
+};
+
+inline ostream& operator <<(ostream& o, const Vecteur2D& v)
+{
+    return o << string(v);
+}
+
+inline Vecteur2D operator *(const double& d, const Vecteur2D& v)
+{
+    return v * d;
+}
+
+
+#endif //VECTEUR2D_H
