468806f6653722eb61168d418f2c520d0f736e6e
Devin PierreEmmanuel <pierre-emmanuel.devin2@etu.univ-lorraine.fr>
2023-12-16 09:57:28 +0100

    Added virtual string() and operator==

--- client/Formes/Forme.h
+++ client/Formes/Forme.h
@@ -21,7 +21,7 @@ private:
 
 public:
   /// Une forme est définie par une couleur
-  explicit Forme(Couleur color) : color(color){};
+  Forme(Couleur color) : color(color){};
 
   virtual ~Forme() = default;
 
@@ -35,18 +35,17 @@ public:
   /// @param point_invariant le point sur lequel va se baser l'homotetie
   /// @param rapport le rapport d'homothetie, il va définir le zoom ou le
   /// dézoom.
-  virtual void homotetie(const Point2D &point_invariant, const double &rapport) = 0;
+  virtual void homotetie(const Point2D &point_invariant,
+                         const double &rapport) = 0;
 
   /// La rotation s'applique sur l'objet implicite.
   /// @param point_invariant le point sur lequel va se baser la rotation
-  /// @param theta l'angle sur lequel va se baser la rotation
-  virtual void rotation(const Point2D &point_invariant, const double &theta) = 0;
+  /// @param point_invariant l'angle sur lequel va se baser la rotation
+  virtual void rotation(const Point2D &point_invariant,
+                        const double &theta) = 0;
 
-  /// For Gtest
-  virtual bool operator==(const Forme&) const = 0;
-
-  /// To help visualize
-  virtual explicit operator string() = 0;
+  virtual bool operator==(const Forme &) const = 0;
+  virtual explicit operator string() const = 0;
 };
 
 #endif // !FORMES_H
--- client/Formes/FormeSimple/Segment.h
+++ client/Formes/FormeSimple/Segment.h
@@ -6,49 +6,43 @@
 #include "../Forme.h"
 #include <cmath>
 
-class Segment : public Forme
-{
+class Segment : public Forme {
 public:
-    Point2D a, b;
-    /// Un segment est définit par 2 points
-    Segment(Couleur color, Point2D a, Point2D b) : Forme(color), a(a), b(b)
-    {
-    }
+  Point2D a, b;
+  /// Un segment est définit par 2 points
+  Segment(Couleur color, Point2D a, Point2D b) : Forme(color), a(a), b(b) {}
 
-    void translation(const Vecteur2D& translation) override
-    {
-        a += translation;
-        b += translation;
-    }
+  void translation(const Vecteur2D &translation) override {
+    a += translation;
+    b += translation;
+  }
 
-    void homotetie(const Point2D& point_invariant, const double& rapport) override
-    {
-        a = Vecteur2D(a.x - point_invariant.x, a.y - point_invariant.y) * rapport;
-        b = Vecteur2D(b.x - point_invariant.x, b.y - point_invariant.y) * rapport;
-    }
+  void homotetie(const Point2D &point_invariant,
+                 const double &rapport) override {
+    a = Vecteur2D(a.x - point_invariant.x, a.y - point_invariant.y) * rapport;
+    b = Vecteur2D(b.x - point_invariant.x, b.y - point_invariant.y) * rapport;
+  }
 
-    void rotation(const Point2D& point_invariant, const double& theta) override
-    {
-        const double ctheta = cos(theta);
-        const double stheta = sin(theta);
+  void rotation(const Point2D &point_invariant, const double &theta) override {
+    const double ctheta = cos(theta);
+    const double stheta = sin(theta);
 
-        a = Matrice22(ctheta, -stheta, stheta, ctheta) * a;
-        b = Matrice22(ctheta, -stheta, stheta, ctheta) * b;
-    }
+    a = Matrice22(ctheta, -stheta, stheta, ctheta) * a;
+    b = Matrice22(ctheta, -stheta, stheta, ctheta) * b;
+  }
 
-    bool operator==(const Forme& f) const override
-    {
-        const Segment* s;
-        if ((s = dynamic_cast<const Segment*>(&f)) == nullptr) return false;
-        return s->a == a && s->b == b;
-    }
+  bool operator==(const Forme &f) const override {
+    const Segment *s;
+    if ((s = dynamic_cast<const Segment *>(&f)) == nullptr)
+      return false;
+    return s->a == a && s->b == b;
+  }
 
-    explicit operator string() override
-    {
-        ostringstream s;
-        s << "Segment [ " << a << ";" << b << " ]";
-        return s.str();
-    }
+  explicit operator string() const override {
+    ostringstream s;
+    s << "Segment [ " << a << ";" << b << " ]";
+    return s.str();
+  }
 };
 
 #endif // !SEGMENT_H
