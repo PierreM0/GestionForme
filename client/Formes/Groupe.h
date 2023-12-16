fae099a26cd7c6e2161565cb03ddeec1c867688f
Devin PierreEmmanuel <pierre-emmanuel.devin2@etu.univ-lorraine.fr>
2023-12-16 14:25:58 +0100

    Ajout des Groupes (aucun test, rien de nouveau)

--- client/Formes/Groupe.h
+++ client/Formes/Groupe.h
@@ -1,58 +1,84 @@
-#ifndef FORME_COMPOSEE_H
-#define FORME_COMPOSEE_H
+//
+// Created by pierrem on 16/12/23.
+//
 
-#include "Formes.h"
+#ifndef GROUPE_H
+#define GROUPE_H
 #include <memory>
 #include <vector>
-class Groupe : public Forme {
-  /// Pointeur unique vers une forme, la forme est virtuelle, et il ne peux y
-  /// avoir qu'une instance par forme
-  vector<unique_ptr<Forme>> formes;
+
+#include "Forme.h"
+
+class  Groupe : public Forme
+{
+
+/// Only one forme of each.
+vector<unique_ptr<Forme>> formes;
 
 public:
-  /// Cree une FormeComposee vide.
-  /// @param color le couleur de la forme.
-  Groupe(Color color) : Forme(color) {}
-
-  /// Ajoutes une forme dans la forme composée.
-  /// @param forme: la forme a ajouter
-  Groupe &add(const unique_ptr<Forme> &forme) {
-    // TODO:
-    // for (const auto &f : formes) {
-    //  if (f->join(forme))
-    //    throw FormeException(FormeException::INTERSECTION_DU_GROUPE);
-    // }
-    formes.push_back(forme);
-    return *this;
-  }
-
-  void translation(const Vecteur2D &translation) override {
-    for (const auto &forme : formes) {
-      forme->translation(translation);
-    }
-  }
-
-  void homotetie(const Point2D &point_invariant,
-                 const double &rapport) override {
-    for (const auto &forme : formes) {
-      forme->homotetie(point_invariant, rapport);
-    }
-  }
-
-  void rotation(const Point2D &point_invariant, const double &theta) override {
-    for (const auto &forme : formes) {
-      forme->rotation(point_invariant, theta);
-    }
-  }
-
-  bool operator==(const Groupe &f) {
-    if (this == &f) return true;
-    for (int i = 0; i < formes.size(); ++i)
+    explicit Groupe(const Couleur &color)
+        : Forme(color)
     {
-      if (f->formes[i] != formes[i]) return false;
     }
-    return true;
-  }
+
+    void add(const unique_ptr<Forme> &f)
+    {
+
+        formes.push_back(f);
+    }
+
+    void translation(const Vecteur2D& translation) override
+    {
+        for (auto f: formes)
+        {
+           f->translation(translation);
+        }
+    }
+    void homotetie(const Point2D& point_invariant, const double& rapport) override
+    {
+        for (auto f: formes)
+        {
+           f->homotetie(point_invariant, rapport);
+        }
+    }
+    void rotation(const Point2D& point_invariant, const double& theta) override
+    {
+        for (auto f: formes)
+        {
+           f->rotation(point_invariant, theta);
+        }
+    }
+
+    bool operator==(const Forme& f) const override
+    {
+        const Groupe *g;
+        if ((g = dynamic_cast<const Groupe*>(&f)) == nullptr)
+         return false;
+        for (auto forme: formes)
+        {
+         bool found = false;
+         for (const auto gforme: g->formes)
+         if (*forme == *gforme) {
+            found = true;
+            break;
+         }
+         if (!found)
+             return false;
+        }
+
+        return true;
+    }
+
+    operator string() const override
+    {
+        ostringstream s;
+        s << "Groupe: " << endl;
+        for (auto forme: formes)
+        {
+            s << "    " << string(*forme) << endl;
+        }
+        return s.str();
+    }
 };
 
-#endif // !FORME_COMPOSEE_H
+#endif //!GROUPE_H
--- client/Tests/Formes/FormeSimple/testTriangle.cpp
+++ client/Tests/Formes/FormeSimple/testTriangle.cpp
@@ -1,38 +1,39 @@
 #include "../../../Formes/FormeSimple/Triangle.h"
+#include <cmath>
 #include <gtest/gtest.h>
 #include <valarray>
-#include <cmath>
 
-TEST(testTriangle, testTranslation)
-{
-    Forme* f = new Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(1, 1), Point2D(0, 1));
-    const Forme* f2 = new Triangle(Forme::Couleur::RED, Point2D(1, 0), Point2D(2, 1), Point2D(1, 1));
-    f->translation(Vecteur2D(1, 0));
+TEST(testTriangle, testTranslation) {
+  Forme *f = new Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(1, 1),
+                          Point2D(0, 1));
+  const Forme *f2 = new Triangle(Forme::Couleur::RED, Point2D(1, 0),
+                                 Point2D(2, 1), Point2D(1, 1));
+  f->translation(Vecteur2D(1, 0));
 
-
-    ASSERT_EQ(*f, *f2);
+  ASSERT_EQ(*f, *f2);
 }
 
-TEST(testTriangle, testHomotetie)
-{
-    Forme* f = new Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(1, 1), Point2D(0, 1));
-    const Forme* f2 = new Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(2, 2), Point2D(0, 2));
-    f->homotetie(Point2D(0, 0), 2.0);
+TEST(testTriangle, testHomotetie) {
+  Forme *f = new Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(1, 1),
+                          Point2D(0, 1));
+  const Forme *f2 = new Triangle(Forme::Couleur::RED, Point2D(0, 0),
+                                 Point2D(2, 2), Point2D(0, 2));
+  f->homotetie(Point2D(0, 0), 2.0);
 
-    ASSERT_EQ(*f, *f2);
+  ASSERT_EQ(*f, *f2);
 }
 
-TEST(testTriangle, testRotation)
-{
-    auto t = Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(1, 1), Point2D(0, 1));
-    const auto t2 = Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(-1, 1), Point2D(-1, 0));
-    t.rotation(Point2D(0, 0), M_PIl/2);
+TEST(testTriangle, testRotation) {
+  auto t = Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(1, 1),
+                    Point2D(0, 1));
+  const auto t2 = Triangle(Forme::Couleur::RED, Point2D(0, 0), Point2D(-1, 1),
+                           Point2D(-1, 0));
+  t.rotation(Point2D(0, 0), M_PIl / 2);
 
-    cout << string(t) << string(t2);
-    ASSERT_NEAR(t.a.x, t2.a.x, 1e-15);
-    ASSERT_NEAR(t.a.y, t2.a.y, 1e-15);
-    ASSERT_NEAR(t.b.x, t2.b.x, 1e-15);
-    ASSERT_NEAR(t.b.y, t2.b.y, 1e-15);
-    ASSERT_NEAR(t.c.x, t2.c.x, 1e-15);
-    ASSERT_NEAR(t.c.y, t2.c.y, 1e-15);
+  ASSERT_NEAR(t.a.x, t2.a.x, 1e-15);
+  ASSERT_NEAR(t.a.y, t2.a.y, 1e-15);
+  ASSERT_NEAR(t.b.x, t2.b.x, 1e-15);
+  ASSERT_NEAR(t.b.y, t2.b.y, 1e-15);
+  ASSERT_NEAR(t.c.x, t2.c.x, 1e-15);
+  ASSERT_NEAR(t.c.y, t2.c.y, 1e-15);
 }
